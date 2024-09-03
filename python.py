f556f97792796025b492a0dde05a540fb45cfd92
Gaboszone <gaboszone39@gmail.com>
2024-09-03 17:52:12 -0400

    Add files via upload

---
+++ python.py
@@ -0,0 +1,14 @@
+# Intro Examples
+# using input() & print() function to display
+
+course  = input("Enter your course: ")
+print("Welcome to " + course + '!')
+
+print("Gabriel Reyes")
+
+height = input("Enter your height")
+
+#Continue to the next section
+
+Diary = input("Dear diary, over the summer I:")
+print("Sounds like a very eventful summer!")
